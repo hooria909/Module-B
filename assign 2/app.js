c0c4df4f567b96904222afc02e1d6a32e05ed110
hooria909 <hooriarameen2@gmail.com>
2025-12-10 14:01:52 +0500

    Add files via upload

---
+++ assign 2/app.js
@@ -0,0 +1,380 @@
+let a = 5;
+
+// 1
+
+let exp1 = ((4 + 5) && "abc" || false + "test") * (a-- || --a) || (false && (++a + 1)) * "end";
+
+// final ans
+// exp1 = NaN
+
+// 2
+
+let exp2 = 10 * ("foo" && 5 + (++a) || "bar") && (false + "test") || 0 && true;
+
+// final ans
+// exp2 = falsetest
+
+// 3
+
+// final ans
+// exp3 = "2value"
+
+// 4
+
+// final ans
+// exp4 = "xyz"
+
+// 5
+
+// final ans
+// exp5 = true
+
+// 6
+
+// final ans
+// exp6 = 11
+
+// 7
+
+// final ans
+// exp7 = 11
+
+// 8
+
+// final ans
+// exp8 = 3
+
+// 9
+
+// final ans
+// exp9 = "null"
+
+// 10
+
+// final ans
+// exp10 = "start"
+
+// 11
+
+// final ans
+// exp11 = 3
+
+// 12
+
+// final ans
+// exp12 = "result"
+
+// 13
+
+// final ans
+// exp13 = "test"
+
+// 14
+
+// final ans
+// exp14 = 4
+
+// 15
+
+// final ans
+// exp15 = "result"
+
+// 16
+
+// final ans
+// exp16 = "start"
+
+// 17
+
+// final ans
+// exp17 = 0
+
+// 18
+
+// final ans
+// exp18 = 0
+
+// 19
+
+// final ans
+// exp19 = "foo"
+
+// 20
+
+// final ans
+// exp20 = "end5"
+
+// 21
+
+// final ans
+// exp21 = "testfalse"
+
+// 22
+
+// final ans
+// exp22 = "test"
+
+// 23
+
+// final ans
+// exp23 = "result"
+
+// 24
+
+// final ans
+// exp24 = 2
+
+// 25
+
+// final ans
+// exp25 = 5
+
+// 26
+
+// final ans
+// exp26 = 12
+
+// 27
+
+// final ans
+// exp27 = "start5"
+
+// 28
+
+// final ans
+// exp28 = "world"
+
+// 29
+
+// final ans
+// exp29 = "NaNresult"
+
+// 30
+
+// final ans
+// exp30 = "falsevalueend"
+
+// 31
+
+// final ans
+// exp31 = "3end"
+
+// 32
+
+// final ans
+// exp32 = 3
+
+// 33
+
+// final ans
+// exp33 = "end5"
+
+// 34
+
+// final ans
+// exp34 = "hello25"
+
+// 35
+
+// final ans
+// exp35 = "falseend"
+
+// 36
+
+// final ans
+// exp36 = 8
+
+// 37
+
+// final ans
+// exp37 = "2test"
+
+// 38
+
+// final ans
+// exp38 = "2end"
+
+// 39
+
+// final ans
+// exp39 = 2
+
+// 40
+
+// final ans
+// exp40 = NaN
+
+// 41
+
+// final ans
+// exp41 = "hello"
+
+// 42
+
+// final ans
+// exp42 = NaN
+
+// 43
+
+// final ans
+// exp43 = "start5"
+
+// 44
+
+// final ans
+// exp44 = "value1"
+
+// 45
+
+// final ans
+// exp45 = 19
+
+// 46
+
+// final ans
+// exp46 = "test"
+
+// 47
+
+// final ans
+// exp47 = 8
+
+// 48
+
+// final ans
+// exp48 = "9hello"
+
+// 49
+
+// final ans
+// exp49 = 32
+
+// 50
+
+// final ans
+// exp50 = ""
+
+// 51
+
+// final ans
+// exp51 = 42
+
+// 52
+
+// final ans
+// exp52 = "9test"
+
+// 53
+
+// final ans
+// exp53 = "3value"
+
+// 54
+
+// final ans
+// exp54 = "7result"
+
+// 55
+
+// final ans
+// exp55 = "start25test"
+
+// 56
+
+// final ans
+// exp56 = "24test"
+
+// 57
+
+// final ans
+// exp57 = NaN
+
+// 58
+
+// final ans
+// exp58 = "7end3"
+
+// 59
+
+// final ans
+// exp59 = "100test"
+
+// 60
+
+// final ans
+// exp60 = "5valuetest4"
+
+// 61
+
+// final ans
+// exp61 = "8result"
+
+// 62
+
+// final ans
+// exp62 = "start16test"
+
+// 63
+
+// final ans
+// exp63 = "7test"
+
+// 64
+
+// final ans
+// exp64 = NaN
+
+// 65
+
+// final ans
+// exp65 = "value15test"
+
+// 66
+
+// final ans
+// exp66 = "12test"
+
+// 67
+
+// final ans
+// exp67 = "start20end"
+
+// 68
+
+// final ans
+// exp68 = "12result"
+
+// 69
+
+// final ans
+// exp69 = "35test"
+
+// 70
+
+// final ans
+// exp70 = "7result"
+
+// 71
+
+// final ans
+// exp71 = "test16"
+
+// 72
+
+// final ans
+// exp72 = "18value"
+
+// 73
+
+// final ans
+// exp73 = "5test"
+
+// 74
+
+// final ans
+// exp74 = "10end"
+
+// 75
+
+// final ans
+// exp75 = "3test"
